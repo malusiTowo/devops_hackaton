fa8fcadc06183883814b2bbcf978a6a25b75a44e
Malusi Towo <malusitowo@gmail.com>
2020-02-12 14:12:37 +0100

    [ADD] add of first routes

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.get('/liste-personnes', (req, res, next) => {
+  res.send('helloc');
+});
 
 
+app.get('/liste-personnes', (req, res, next) => {
+  res.send('helloc')
+});
+
+// app.get('/bad', (req,res) => )
 app.listen(port, () => console.log('Server is listening on port ' + port));
