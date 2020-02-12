a4967c9d9db252a12a29908876eb936af0043c18
Malusi Towo <malusitowo@gmail.com>
2020-02-12 15:40:57 +0100

    [ADD] setup of prometheus

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-import express from 'express';
-
+const express = require('express');
+const Prometheus = require('./utils/prometheus');
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(Prometheus.requestCounters);
+app.use(Prometheus.responseCounters);
+
+/**
+ * Enable metrics endpoint
+ */
+Prometheus.injectMetricsRoute(app);
+
+/**
+ * Enable collection of default metrics
+ */
+Prometheus.startCollection();
+
 
 app.get('/liste-personnes', (req, res, next) => {
   res.send('helloc');
 });
 
+app.get('/', (req, res) => res.send('home'));
 
-app.get('/liste-personnes', (req, res, next) => {
-  res.send('helloc')
+app.get('/bad', (req, res) => {
+  throw new Error();
 });
 
-// app.get('/bad', (req,res) => )
+
 app.listen(port, () => console.log('Server is listening on port ' + port));
